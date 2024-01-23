6c5979b1ebcd886237fcddfe36886ae8d3154096
rishi <rishiraj.9886@gmail.com>
2024-01-24 00:07:05 +0530

    last

--- src/app/component/LoginPage.tsx
+++ src/app/component/LoginPage.tsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
 
   const handleGoogleLogin = async () => {
     try {
-      await signIn('google', { callbackUrl:'/' });
+      await signIn('google', {redirect:false});
     } catch (error) {
       console.error('Google sign-in error:', error);
     }
